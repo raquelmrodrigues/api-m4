1dbb48efe44a6e1886a77d3606e48d1fad52efb0
Raquel Rodrigues <raquel.mr.rodrigues@gmail.com>
2022-06-24 11:57:23 -0300

    aluno model

---
+++ src/models/aluno-model.js
@@ -0,0 +1,13 @@
+class Aluno {
+    constructor(nome, idade, telefone, email, senha, turma) {
+
+        this.nome = nome
+        this.idade = idade
+        this.telefone = telefone
+        this.email = email
+        this.senha = senha
+        this.turma = turma
+    }
+}
+
+module.exports = Aluno;
